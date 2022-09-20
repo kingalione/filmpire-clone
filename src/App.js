24686f749e539e7061359351686c91b5e27673f2
akaradag <a.karadag@5flow.eu>
2022-09-20 15:34:12 +0200

    clean up index.js to new createRoot structure
    - add fetching basics of omdb api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import { useEffect } from 'react';
+
+//omdb api key
+
+const API_URL = 'http://www.omdbapi.com?apikey=c34a95a8';
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data);
+    }
+
+    useEffect(() => {
+        searchMovies('Batman')
+    }, [])
+
     return (
         <h1>App</h1>
     )
