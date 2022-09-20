a60212a64a59ccabb736c4ff18465caaefd812d8
akaradag <a.karadag@5flow.eu>
2022-09-20 18:06:04 +0200

    finish the list view
    - add states
    - add onchange function
    - add onkeyup function for enter press in a input field

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import MovieCard from './components/MovieCard';
 import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=c34a95a8';
 
-const movie1 = {
-    "Title": "The Batman",
-    "Year": "2022",
-    "imdbID": "tt1877830",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_SX300.jpg"
-}
-
 const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data.Search);
+        setMovies(data.Search);
     }
 
     useEffect(() => {
@@ -33,18 +27,31 @@ const App = () => {
             <div className='search'>
                 <input
                     placeholder='Search for movies'
-                    value='Superman'
-                    onChange={() => { }} />
+                    value={searchTerm}
+                    onChange={e => setSearchTerm(e.target.value)}
+                    onKeyUp={e => {
+                        if (e.key === 'Enter') {
+                            searchMovies(searchTerm)
+                        }
+                    }}
+                />
                 <img
                     src={SearchIcon}
                     alt='Search'
-                    onClick={() => { }}
+                    onClick={() => { searchMovies(searchTerm) }}
                 />
             </div>
 
-            <div className='container'>
-                <MovieCard movie1={movie1}/>
-            </div>
+            {movies?.length > 0 ? (
+                <div className='container'>
+                    {movies.map(movie => <MovieCard movie={movie} />)}
+                </div>
+            ) : (
+                <div className='empty'>
+                    <h2>No movies found</h2>
+                </div>
+            )}
+
         </div>
     )
 }
